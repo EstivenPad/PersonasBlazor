b9b3db8890ed6acaebbb9fb4d9ce5b6f791363f6
Estiven De Jesus Padilla Santos <estivenps26@gmail.com>
2020-05-23 17:08:16 -0400

    Agregando validación de tamaño de la cadena

--- PersonasBlazor/Models/Personas.cs
+++ PersonasBlazor/Models/Personas.cs
@@ -14,11 +14,13 @@ namespace PersonasBlazor.Models
         [Required(ErrorMessage = "Es obligatorio introducir un nombre")]
         public string Nombre { get; set; }
 
-        [Phone(ErrorMessage ="El teléfono debe tener el siguiente formato: 999-999-9999")] 
+        [Phone(ErrorMessage ="El teléfono debe tener el siguiente formato: 999-999-9999")]
+        [StringLength(14, ErrorMessage = "El teléfono debe tener el tamaño correcto", MinimumLength = 13)]
         [Required(ErrorMessage = "Es obligatorio introducir un telefono")]
         public string Telefono { get; set; }
 
         [Required(ErrorMessage = "Es obligatorio introducir una cédula")]
+        [StringLength(13, ErrorMessage = "La cédula debe tener el tamaño correcto", MinimumLength = 13)]
         public string Cedula { get; set; }
 
         [Required(ErrorMessage = "Es obligatorio introducir una dirección")]
